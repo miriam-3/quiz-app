401a9195f8b5b0d4a2030d6a723feff1877f115f
miriam-3 <131079215+miriam-3@users.noreply.github.com>
2023-04-17 23:46:22 +0000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+
+
+
+let questions = [{
+  numb: 1,
+  question: "How many players play a football martch?",
+  answer: "11", options: [
+    "11", 
+    "10", 
+    "9",
+    "12"
+  ]
+},
+{
+  numb: 2,
+  question: "How many tooth does a human have?",
+  answer: "32",
+  options: [
+    "20",
+    "42",
+    "32",
+    "None of the above"
+  ]
+},
+{
+  numb: 3,
+  question: "What are the types of gender?",
+  answer: "female, male",
+  options: [
+    "female, male",
+    "male, female, trans",
+    "Both",
+    "None of the above"
+  ]
+},
+{
+  numb: 4,
+  question: "What age in Ghana shows you are an adult?",
+  answer: "18+",
+  options: [
+    "30+",
+    "40+",
+    "16+",
+    "18+"
+  ]
+},
+{
+  numb: 5,
+  question: "What language is spoken in Ghana",
+  answer: "Twi",
+  options: [
+    "Twi",
+    "Latin",
+    "French",
+    "All the above"
+  ]
+},
+{
+  numb: 6,
+  question: "What are are the colours of the Ghana flag?",
+  answer: "Red, green, yellow, black",
+  options: [
+    "Green white black",
+    "Red, yellow, black, green, brown",
+    "Red, yellow, green, brown",
+    "Red, green, yellow, black"
+  ]
+},
+{
+  numb: 7,
+  question: "How many region do Ghana have?",
+  answer: "16",
+  options: [
+    "10",
+    "12",
+    "14",
+    "16"
+  ]
+},
+{
+  numb: 8,
+  question: "How many local districts does Ghana have?",
+  answer: "216",
+  options: [
+    "200",
+    "210",
+    "216",
+    "220"
+  ]
+},
+{
+  numb: 9,
+  question: "when did Ghana establish the former ten regions?",
+  answer: "1987",
+  options: [
+    "1957",
+    "1987",
+    "1960",
+    "1844"
+  ]
+},
+{
+  numb: 10,
+  question: "How was Ghana called back in the days?",
+  answer: "Gold caost",
+  options: [
+    "Ghana empire",
+    "Ghana",
+    "Gold caost",
+    "None of the above"
+  ]
+}
+];
